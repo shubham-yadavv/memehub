d37c5325a8730dc060b74bfdb07378382c617f16
shubham <shubham22121@gmail.com>
2021-11-01 03:11:03 +0530

    build an docker image

--- meme/views.py
+++ meme/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 class Index(View):
     def get(self, request):
         return render(request, 'index.html')
 
-        
+
--- memehub/settings.py
+++ memehub/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'memehub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
